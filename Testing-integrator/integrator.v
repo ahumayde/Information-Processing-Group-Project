d17c75991f045d0efa73e34c076ab24dbaff1782
Omar Alkhatib <oa321@ic.ac.uk>
2023-02-25 20:19:03 +0000

    integrator: not working yet, verilog of integrator

--- Testing-integrator/integrator.v
+++ Testing-integrator/integrator.v
@@ -1,34 +1,39 @@
-module integrator #( 
-    CLOCK_SPEED = 50000000
-)(
-    input         clk, 
-    input         clk_en,
-    input         start,
-    input         reset,
-    input  [31:0] dataA, // Accelerometer
-    input  [31:0] dataB, // OLD SUM
-    output        done,
-    output [31:0] result // NEW SUM
-);
+module integrator(input  wire  clk,
+	         input  wire  clk_en,
+			 input  wire  reset,
+			 input  wire  start,
+			 output reg   done,
+			 input  wire  [31:0] dataa,
+			 input  wire  [31:0] datab,
+			 output reg   [31:0] result);
+    
+  reg count;
+  initial count = 0;
+  wire [31 : 0] TIME_PERIOD;
+  assign TIME_PERIOD = 1 / 5000000;
+  reg [31 : 0] dataa_old;
 
-    wire [31:0] TIME_PERIOD = 1 / CLOCK_SPEED;
-    wire [31:0] dataA_old;
-    wire done;
-    wire count;
-
-if (start == 1) begin 
-    done = 0;
-    count = 0;
-end
-
-always @(posedge clk ) begin
-    if (count == 0)
-        dataA_old = dataA;
-    else begin
-        result = dataB + TIME_PERIOD * ((dataA + dataA_old)/2);
-        done = 1;
+  always @(posedge clk or posedge reset)
+  begin
+    if (reset) 
+    begin
+      count <= 1'h0;	
+      done <= 1'b0;
     end
-    count += 1;
-end     
+    else begin
+	  done <= 1'b0;
+	  if (clk_en & start & (count == 0))
+	      begin
+            dataa_old <= dataa;
+            count <= count + 1;
+            done <= 1'b0;
+          end
+      else if (clk_en & start & (count == 1))
+          begin
+            result <= datab + TIME_PERIOD * ((dataa + dataa_old)/2);
+            done <= 1'b1;
+          end
+      end
+    end
 
-endmodule 
+endmodule
