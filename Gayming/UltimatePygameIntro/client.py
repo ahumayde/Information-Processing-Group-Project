35ee0477a248070cc5b8f44a6915f26f266511fb
elkouny <108074024+elkouny@users.noreply.github.com>
2023-02-28 16:00:34 +0000

    html test

--- Gayming/UltimatePygameIntro/client.py
+++ Gayming/UltimatePygameIntro/client.py
@@ -1,9 +1,9 @@
-# import requests as rq
-#
-# x = rq.get('http://127.0.0.1:5000')
-# print(x.text)
-#
-# y = rq.get('http://127.0.0.1:5000/ahmad')
-# print(y.text)
-#
-# rq.post('http://127.0.0.1:5000/')
+import requests as rq
+import time
+jsurl="http://127.0.0.1:5000"
+while True:
+    x = rq.get(jsurl)
+    print(x.text)
+    time.sleep(1)
+
+
--- Gayming/UltimatePygameIntro/top.py
+++ Gayming/UltimatePygameIntro/top.py
@@ -1,6 +1,11 @@
-import subprocess
-
-subprocess.run(["server.py"])
-subprocess.run(["game_test"])
 
 # call functions or execute code from the imported modules as needed
+import os
+
+npath = "/Users/ahmedelkouny/PycharmProjects/pythonProject1/Information-Processing-Group-Project/Gayming/UltimatePygameIntro/server.py"
+
+kpath = "/Users/ahmedelkouny/PycharmProjects/pythonProject1/Information-Processing-Group-Project/Gayming/UltimatePygameIntro/game_test.py"
+
+os.startfile(npath)
+
+os.startfile(kpath)
