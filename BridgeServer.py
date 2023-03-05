2db4530d2cfccd39c798088fc5ca010be358efd2
headzy <ah121@ic.ac.uk>
2023-03-05 16:54:58 +0000

    cleaned up Bridge Server and added comments for GET requests

--- BridgeServer.py
+++ BridgeServer.py
@@ -1,12 +1,13 @@
 import socket
-import time
 import requests
 import threading
-import json 
+# import json 
 
-URL = "http://172.20.10.11:8080/data"
+URL = "http://172.20.10.11:8080"
 port = 10000
 payload = { "right" : 0, "left" : 0, "drop" : 0, "store" : 0}
+UpperBoundY =  20
+LowerBoundY = -20
 
 def connect():
     wsocket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,78 +19,73 @@ def connect():
     print("Connection: ", address)
     return csocket
 
-# def process(variable ,data):
-#     global payload
-#     payload[variable] = 2*int(data)
+def process(x, y, b):
+    if b == 1:
+        # Post
+        payload["right"] = 1
+        payload["left"]  = 0
+        # Get
+        # r = requests.get(URL+"/right")
+    elif b == 2:
+        # Post
+        payload["right"] = 0
+        payload["left"]  = 1
+        # Get
+        # r = requests.get(URL+"/left")
+    else: # Post
+        payload["right"] = 0
+        payload["left"]  = 0
+    if y > UpperBoundY:
+        # Post
+        payload["drop"]  = 1
+        payload["store"] = 0
+        # Get
+        # r = requests.get(URL+"/drop")
+    elif y < LowerBoundY:
+        # Post
+        payload["drop"]  = 0
+        payload["store"] = 1
+        # Get
+        # r = requests.get(URL+"/store")
+    else: # Post
+        payload["drop"]  = 0
+        payload["store"] = 0
 
-# def recvMsg(csocket):
-#     while True:
-#         data = csocket.recv(1024).decode()
-#         if len(data) != 0:
-#             print("Data Received: ", data)
-#             process("x",data)
+    print("x:", x, " y:",y," b:",b)
+    print(payload)
+    
 
-# def postReq():
-#     while True:
-#         print("trying post")
-#         r = requests.post(URL, data=payload)
-#         # r_dict = r.json()
-#         # print(r_dict['data'], r_dict['form'])
-#         print("successfull post")
-
-def main():
-    global payload
-    csocket = connect()
-
+def recieve_message(csocket):
     while True:
         msg = csocket.recv(1024).decode()
-        # payload["x"] = msg
         x = int(msg.split(",")[1])
         y = int(msg.split(",")[2])
         b = int(msg.split(",")[3])
+        process(x, y, b)
 
-        if b == 1:
-            payload["right"] = 1
-            payload["left"]  = 0
-        elif b == 2:
-            payload["right"] = 0
-            payload["left"]  = 1
-        else:
-            payload["right"] = 0
-            payload["left"]  = 0
-        if y > 10:
-            payload["drop"]  = 1
-            payload["store"] = 0
-        elif y < -10:
-            payload["drop"]  = 0
-            payload["store"] = 1
-        else:
-            payload["drop"]  = 0
-            payload["store"] = 0    
 
-        print("x:", x, " y:",y," b:",b)
-        print(payload)
-        
-        r = requests.post(URL, data=payload, headers={"Content-Type" : "application/json"})
-        print(r.text)   #_dict['data'], r_dict['form'])
-        print("successfull post")
+def post_request():
+    while True:
+        r = requests.post((URL+"/data"), data=payload, headers={"Content-Type" : "application/json"})
+        #r_dict = r.json()
+        print(r.text) #_dict['data'], r_dict['form'])
 
-        # if len(data) != 0:
-        #     print("Data Received: ", data)
-        #     process('x',data)
-        # print("trying post")
-        # r_dict = jsonify(payload)
-        # r = urllib3.req
-        # r = requests.get(URL)
+def main():
+    # Connect to TCP Client
+    csocket = connect()
 
-    # thread1 = threading.Thread(target=recvMsg, args=(csocket,))
-    # thread2 = threading.Thread(target=postReq)
+    # Get Requests
+    # recieve_message(csocket)        
 
-    # thread1.start()
-    # thread2.start()
+    # Post Requests with threading
+    thread1 = threading.Thread(target=recieve_message, args=(csocket,))
+    thread2 = threading.Thread(target=post_request)
 
-    # thread1.join()
-    # thread2.join()
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
 
 if __name__ == '__main__':
     main()
