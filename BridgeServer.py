89a67ae352495fd97ad4e724f89cd832947743b9
Headzy <ah121@ic.ac.uk>
2023-03-06 11:11:33 +0000

    Added hold down feature and improved algorithm with comments

--- BridgeServer.py
+++ BridgeServer.py
@@ -1,13 +1,22 @@
 import socket
+import time
 import requests
 import threading
-# import json 
+import random
+import json 
 
-URL = "http://172.20.10.11:8080"
+URL = "http://172.20.10.7:8080"
 port = 10000
 payload = { "right" : 0, "left" : 0, "drop" : 0, "store" : 0}
-UpperBoundY =  20
-LowerBoundY = -20
+
+upper_bound_y =  30
+lower_bound_y = -30
+hold_time = 500000000 # in nanoseconds (0.5s)
+
+right_held = False
+left_held  = False
+store_held = False
+drop_held  = False
 
 def connect():
     wsocket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,41 +28,48 @@ def connect():
     print("Connection: ", address)
     return csocket
 
+# Later use x for left and right, with buttons for rotate
 def process(x, y, b):
+    # Right
     if b == 1:
-        # Post
-        payload["right"] = 1
-        payload["left"]  = 0
-        # Get
-        # r = requests.get(URL+"/right")
+        r = requests.get(URL+"/right") # Get
+        # payload["right"] = 1         # Post
+        # payload["left"]  = 0         # Post
+    # Left
     elif b == 2:
-        # Post
-        payload["right"] = 0
-        payload["left"]  = 1
-        # Get
-        # r = requests.get(URL+"/left")
-    else: # Post
-        payload["right"] = 0
-        payload["left"]  = 0
-    if y > UpperBoundY:
-        # Post
-        payload["drop"]  = 1
-        payload["store"] = 0
-        # Get
-        # r = requests.get(URL+"/drop")
-    elif y < LowerBoundY:
-        # Post
-        payload["drop"]  = 0
-        payload["store"] = 1
-        # Get
-        # r = requests.get(URL+"/store")
-    else: # Post
-        payload["drop"]  = 0
-        payload["store"] = 0
+        r = requests.get(URL+"/left")  # Get
+        # payload["right"] = 0         # Post
+        # payload["left"]  = 1         # Post
+    # Store
+    while store_held:
+        # payload["store"] = 0         # Post
+        store_held = y < lower_bound_y
+        if time.time_ns() - start_store >= hold_time:
+            store_held = False
+    if y < lower_bound_y:
+        start_store = time.time_ns()
+        store_held = True
+        r = requests.get(URL+"/store") # Get
+        # payload["drop"]  = 0         # Post
+        # payload["store"] = 1         # Post
+    # Drop
+    while drop_held:
+        # payload["drop"]  = 0         # Post
+        drop_held = y > upper_bound_y 
+        if time.time_ns - start_drop >= hold_time:
+            drop_held = False 
+    if y > upper_bound_y:
+        start_drop = time.time_ns()
+        drop_held = True
+        r = requests.get(URL+"/drop")  # Get
+        print("get sent")              # Get
+        # payload["drop"]  = 1         # Post
+        # payload["store"] = 0         # Post
+        # print("post sent")           # Post
+    # print("x:", x, " y:",y," b:",b)
+    # print(payload)
+    # time.sleep(1)
 
-    print("x:", x, " y:",y," b:",b)
-    print(payload)
-    
 
 def recieve_message(csocket):
     while True:
@@ -66,9 +82,10 @@ def recieve_message(csocket):
 
 def post_request():
     while True:
-        r = requests.post((URL+"/data"), data=payload, headers={"Content-Type" : "application/json"})
-        #r_dict = r.json()
-        print(r.text) #_dict['data'], r_dict['form'])
+        r = requests.post((URL+"/data"), data=payload, headers={"heldent-Type" : "application/json"})
+        # print("post request sent:",r.text)
+        #time.sleep(0.25)
+
 
 def main():
     # Connect to TCP Client
