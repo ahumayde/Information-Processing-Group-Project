7d2b0b3bdf258adfbb56e391c8c1e23c81c150a7
Headzy <ah121@ic.ac.uk>
2023-03-07 13:13:36 +0000

    added option for threading get requests

--- BridgeServer.py
+++ BridgeServer.py
@@ -7,6 +7,7 @@ import json
 
 URL = "http://172.20.10.7:8080"
 port = 10000
+path = "/null"
 payload = { "right" : 0, "left" : 0, "drop" : 0, "store" : 0}
 
 upper_bound_y =  30
@@ -36,18 +37,21 @@ def connect():
 def process(x, y, b):
     # Right
     while right_held:
+        path = "/null"                        # Get Thread
         # payload["right"] = 0                # Post
         right_held = x > upper_bound_x        
         if time.time_ns() - start_right >= hold_time:
             right_held = False                
     if x > upper_bound_x:                     
         start_right = time.time_ns()          
-        right_held = True                      
-        r = requests.get(URL+"/right")        # Get
+        right_held = True 
+        path = "/right"                       # Get Thread
+        # r = requests.get(URL+"/right")      # Get
         # payload["right"] = 1                # Post
         # payload["left"]  = 0                # Post
     # Left
     while left_held:
+        path = "/null"                        # Get Thread
         # payload["left"] = 0                 # Post
         left_held = x < lower_bound_x
         if time.time_ns() - start_left >= hold_time:
@@ -55,11 +59,13 @@ def process(x, y, b):
     if x < lower_bound_x:
         start_left = time.time_ns()
         left_held = True
-        r = requests.get(URL+"/left")         # Get
+        path = "/left"                        # Get Thread
+        # r = requests.get(URL+"/left")       # Get
         # payload["right"] = 0                # Post
         # payload["left"]  = 1                # Post
     # Store
     while store_held:
+        path = "/null"                        # Get Thread
         # payload["store"] = 0                # Post
         store_held = y < lower_bound_y
         if time.time_ns() - start_store >= hold_time:
@@ -67,29 +73,34 @@ def process(x, y, b):
     if y < lower_bound_y:
         start_store = time.time_ns()
         store_held = True
-        r = requests.get(URL+"/store")        # Get
+        path = "/store"                       # Get Thread
+        # r = requests.get(URL+"/store")      # Get
         # payload["drop"]  = 0                # Post
         # payload["store"] = 1                # Post
     # Drop                                    
-    while drop_held:                          
-        # payload["drop"]  = 0                # Post
+    while drop_held:     
+        path = "/null"                        # Get Thread                    
+        # payload["drop"] = 0                 # Post
         drop_held = y > upper_bound_y         
         if time.time_ns - start_drop >= hold_time:
             drop_held = False                 
     if y > upper_bound_y:                     
         start_drop = time.time_ns()           
-        drop_held = True                      
-        r = requests.get(URL+"/drop")         # Get
+        drop_held = True   
+        path = "/drop"                        # Get Thread                   
+        # r = requests.get(URL+"/drop")       # Get
         print("get sent")                     # Get
         # payload["drop"]  = 1                # Post
         # payload["store"] = 0                # Post
         # print("post sent")                  # Post
     # Rotate Right                            
-    if b == 1:                                
-        r = requests.get(URL+"/rotate_right") # Get
+    if b == 1:          
+        path = "/rotate_right"                  # Get Thread                      
+        # r = requests.get(URL+"/rotate_right") # Get
     # Rotate Left
     if b == 2:
-        r = requests.get(URL+"/rotate_left")  # Get
+        path = "/rotate_left"                   # Get Thread
+        # r = requests.get(URL+"/rotate_left")  # Get 
     # print("x:", x, " y:",y," b:",b)
     # print(payload)
 
@@ -101,6 +112,11 @@ def recieve_message(csocket):
         y = int(msg.split(",")[2])
         b = int(msg.split(",")[3])
         process(x, y, b)
+
+
+def get_request():
+    while True:
+        r = requests.get(URL+path)
 
 
 def post_request():
@@ -119,7 +135,8 @@ def main():
 
     # Post Requests with threading
     thread1 = threading.Thread(target=recieve_message, args=(csocket,))
-    thread2 = threading.Thread(target=post_request)
+    thread2 = threading.Thread(target=get_request)     # Get
+    # thread2 = threading.Thread(target=post_request)  # Post
 
     thread1.start()
     thread2.start()
